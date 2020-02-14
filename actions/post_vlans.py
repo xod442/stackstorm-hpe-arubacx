6bd70a55b2df42614df100a8859a66622ceb876f
Rick Kauffman <Rick@rickkauffman.com>
2020-02-14 10:53:00 -0800

    beta

---
+++ actions/post_vlans.py
@@ -0,0 +1,45 @@
+# (C) Copyright 2019 Hewlett Packard Enterprise Development LP.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#  http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# __author__ = "@netwookie"
+# __credits__ = ["Rick Kauffman"]
+# __license__ = "Apache2.0"
+# __maintainer__ = "Rick Kauffman"
+# __email__ = "rick.a.kauffman@hpe.com"
+
+from lib.actions import ArubaCxBaseAction
+import json
+
+class PostVlan(ArubaCxBaseAction):
+    def run(self, name=None, description=None, id=None, admin=None):
+        # Send clan to the AurbaCX switch
+        params={}
+        params['name'] = name
+        params['description'] = description
+        params['id'] = id
+        params['admin'] = admin
+
+        # create the vlans url
+        vlan_url = self.base + '/system/vlans'
+        # Post to the Aruba CX switch
+        response = self.session.post(url=vlan_url,params=params,verify=False)
+        # Validate response
+        if response.status_code == 201:
+            return(True, result.status_code)
+        return (False, result.status_code)
+
+        # Logout of the session
+        url =  self.base + '/logout'
+        response = self.session.post(url=url,cookies=self.cookie,verify=False)
+        return (True, vlans)
